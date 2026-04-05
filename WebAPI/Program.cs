ad1926f9003adf8f4664751d7d95ddcef4dc41c8
Данииль <daniilfarsatov@gmail.com>
2026-04-05 19:02:15 +0500

    feat: add Docker Compose with PostgreSQL and automatic migrations

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -78,4 +78,10 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await dbContext.Database.MigrateAsync();
+}
+
 app.Run();
