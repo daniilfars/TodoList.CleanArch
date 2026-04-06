d92a1866d51870c63fd000a4ef21f18b8d6948ef
Данииль <daniilfarsatov@gmail.com>
2026-04-06 18:44:05 +0500

    ci: add GitHub Actions workflow for build, test and Docker push

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -80,8 +80,20 @@ app.MapControllers();
 
 using (var scope = app.Services.CreateScope())
 {
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    await dbContext.Database.MigrateAsync();
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        if (context.Database.GetPendingMigrations().Any())
+        {
+            context.Database.Migrate();
+        }
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ошибка при накатывании миграций");
+    }
 }
 
 app.Run();
